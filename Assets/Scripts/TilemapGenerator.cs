36001da8641101cc3a0b63d4af201da7e2e03377
Mourette Ferdinand <mouretteferdinand@yahoo.com>
2026-04-26 22:14:15 +0200

    Start

--- Assets/Scripts/TilemapGenerator.cs
+++ Assets/Scripts/TilemapGenerator.cs
@@ -4,20 +4,18 @@ using UnityEngine.Tilemaps;
 public class TilemapGenerator : MonoBehaviour
 {
     public Tilemap tilemap;
-    public Tile BaseTile;
+    public Tile UncheckedTile,CheckedTile;
 
     public int width = 80;
     public int height = 40;
+    public int numberOfRooms = 2;
     public int seed;
 
     public int averageRoomSize = 5;
 
     void Start() => Generate();
 
-    private void OnValidate()
-    {
-        Generate();
-    }
+
 
     public void Generate()
     {
@@ -28,18 +26,78 @@ public class TilemapGenerator : MonoBehaviour
         for (int x = 0; x < width; x++)
             for (int y = 0; y < height; y++)
             {
-                TileBase tile = BaseTile;
+                TileBase tile = UncheckedTile;
                 tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         
-        bool foundRoom = false;
-        while (!foundRoom)
+        for(int i = 0 ; i < numberOfRooms; i++)
         {
-            int randX = Random.Range(0, width);
-            int randY = Random.Range(0, height); ;
+            RoomData newRoom = new RoomData();
+            newRoom.roomId = i;
+            newRoom.roomType = RoomType.simple;
+
+            bool foundRoom = false;
+            while (!foundRoom)
+            {
+                int randX = Random.Range(0, width);
+                int randY = Random.Range(0, height);
+                Vector2 newPos = new Vector2(randX, randY);
+                int randSize = Random.Range(0, 4);
+                newRoom.roomSizeX = averageRoomSize + randSize;
+                newRoom.roomSizeY = averageRoomSize - randSize;
+                foundRoom = CheckIfRoomFit(newRoom);
+            }
+
+            BuildRoom(newRoom);
         }
+
 
     }
 
 
+    private bool CheckIfRoomFit(RoomData roomData)
+    {
+        for (int x = 0; x < roomData.roomSizeX; x ++)
+        {
+            for (int y = 0; y < roomData.roomSizeY; y++)
+            {
+                Vector3Int newPosTile = new Vector3Int(roomData.pos.x + x, roomData.pos.y + y,0);
+                if (tilemap.GetTile(newPosTile) != UncheckedTile)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void BuildRoom(RoomData roomData)
+    {
+        for (int x = 0; x < roomData.roomSizeX; x++)
+        {
+            for (int y = 0; y < roomData.roomSizeY; y++)
+            {
+                Vector3Int newPosTile = new Vector3Int(roomData.pos.x + x, roomData.pos.y + y, 0);
+                tilemap.SetTile(newPosTile, CheckedTile);
+            }
+        }
+    }
 }
+
+public class RoomData
+{
+    public int roomId; 
+    public int roomSizeX;
+    public int roomSizeY;
+    public Vector2Int pos;
+    public RoomType roomType;
+
+}
+public enum RoomType
+{
+    simple,
+    startVillage,
+    end
+}
+
